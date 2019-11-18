096318c9006a9e25517f13a0bcea8247c5d9a84e
ryann4 <ryannff4@gmail.com>
2019-11-17 23:40:45 -0800

    added data member to tile object to keep track of it has been explored or not; changed render functions to only draw tiles that are within fov or tiles that have already been explored - tiles not within FOV but have been explored look dark

--- render_functions.py
+++ render_functions.py
@@ -21,7 +21,10 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
                     # if does not block sight, draw as a floor
                     else:
                         libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
-                else:  # tile is not visible
+
+                    # mark tiles within the player's field of view as explored
+                    game_map.tiles[x][y].explored = True
+                elif game_map.tiles[x][y].explored:  # tile is not within FOV - however, if the tile has already been explored, draw it as a "dark" tile
                     # if blocks sight, draw as a wall
                     if wall:
                         libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
@@ -41,6 +44,7 @@ def clear_all(con, entities):
 
 
 def draw_entity(con, entity, fov_map):
+    # if the entity is within fov, draw it
     if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
         libtcod.console_set_default_foreground(con, entity.color)
         libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
