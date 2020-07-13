85d6fb3efc123bddc28bf1e2ceb187ce85a6685c
ryann4 <ryannff4@gmail.com>
2020-07-12 20:36:56 -0700

    fixed whitespace; changed item generation to have a chance of generating an item to cast lightning

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,7 +1,7 @@
 import tcod as libtcod
 
 from components.item import Item
-from item_functions import heal
+from item_functions import heal, cast_lightning
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 from random import randint
@@ -125,9 +125,14 @@ class GameMap:
             y = randint(room.y1 + 1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                item_component = Item(use_function=heal, amount=4)
-                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+                item_chance = randint(0, 100)
 
+                if item_chance < 70:
+                    item_component = Item(use_function=heal, amount=4)
+                    item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+                else:
+                    item_component = Item(use_function=cast_lightning, damage=20, maximum_range=5)
+                    item = Entity(x, y, '#', libtcod.yellow, 'Lightning Scroll', render_order=RenderOrder.ITEM, item=item_component)
                 entities.append(item)
 
 
