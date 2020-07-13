04a86d974e6895403614e420d419cda1fcde69cd
ryann4 <ryannff4@gmail.com>
2020-07-12 20:32:56 -0700

    created a method to cast a ranged spell

--- item_functions.py
+++ item_functions.py
@@ -19,3 +19,36 @@ def heal(*args, **kwargs):
                         'message': Message('Your wounds start to feel better!')
                         })
     return results
+
+
+def cast_lightning(*args, **kwargs):
+    caster = args[0]
+    entities = kwargs.get('entities')
+    fov_map = kwargs.get('fov_map')
+    damage = kwargs.get('damage')
+    maximum_range = kwargs.get('maximum_range')
+
+    results = []
+
+    target = None
+    closest_distance = maximum_range + 1
+
+    for entity in entities:
+        if entity.fighter and entity != caster and libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+            distance = caster.distance_to(entity)
+
+            if distance < closest_distance:
+                target = entity
+                closest_distance = distance
+
+    if target:
+        results.append({'consumed': True,
+                        'target': target,
+                        'message': Message('A lightning bolt strikes the {0} with a loud thunder! The damage is {1}'.format(target.name, damage))})
+        results.extend(target.fighter.take_damage(damage))
+    else:
+        results.append({'consumed': False,
+                        'target': None,
+                        'message': Message('No enemy is close enough to strike.', libtcod.red)})
+
+    return results
